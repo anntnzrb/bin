c22fb166feedcd9f28b9778567039c2e38f0b65d
anntnzrb <anntnzrb@proton.me>
2024-09-20 13:22:20 -0500

    fmt: use nixfmt

--- nix/modules/formatter.nix
+++ nix/modules/formatter.nix
@@ -1,37 +1,41 @@
-{ inputs
-, ...
-}: {
+{
+  inputs,
+  ...
+}:
+{
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem = { pkgs, ... }: {
-    treefmt.config = {
-      projectRootFile = "flake.nix";
-      programs = {
-        nixpkgs-fmt.enable = true;
-        actionlint.enable = true;
-        prettier.enable = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt.config = {
+        projectRootFile = "flake.nix";
+        programs = {
+          nixfmt.enable = true;
+          actionlint.enable = true;
+          prettier.enable = true;
 
-        deadnix = {
-          enable = true;
-          no-underscore = true;
+          deadnix = {
+            enable = true;
+            no-underscore = true;
+          };
+
+          statix = {
+            enable = true;
+            disabled-lints = [ "repeated_keys" ];
+          };
         };
 
-        statix = {
-          enable = true;
-          disabled-lints = [ "repeated_keys" ];
+        settings.formatter.shfmt = {
+          command = pkgs.shfmt;
+          includes = [ "*.sh" ];
+          options = [
+            "--simplify"
+            "--binary-next-line"
+            "--indent"
+            "4"
+          ];
         };
-      };
-
-      settings.formatter.shfmt = {
-        command = pkgs.shfmt;
-        includes = [ "*.sh" ];
-        options = [
-          "--simplify"
-          "--binary-next-line"
-          "--indent"
-          "4"
-        ];
       };
     };
-  };
 }
